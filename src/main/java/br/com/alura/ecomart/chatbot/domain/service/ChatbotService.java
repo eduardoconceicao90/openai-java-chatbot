c72ea394b9e6d7ae15dcb0d1bdb3dcdc2cea25b7
Eduardo Conceição <eduardosaconceicao@gmail.com>
2024-03-08 14:08:25 -0300

    Respostas com Stream

--- src/main/java/br/com/alura/ecomart/chatbot/domain/service/ChatbotService.java
+++ src/main/java/br/com/alura/ecomart/chatbot/domain/service/ChatbotService.java
@@ -2,6 +2,8 @@ package br.com.alura.ecomart.chatbot.domain.service;
 
 import br.com.alura.ecomart.chatbot.infra.openai.DadosRequisicaoChatCompletion;
 import br.com.alura.ecomart.chatbot.infra.openai.OpenAIClient;
+import com.theokanning.openai.completion.chat.ChatCompletionChunk;
+import io.reactivex.Flowable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,7 +13,7 @@ public class ChatbotService {
     @Autowired
     private OpenAIClient client;
 
-    public String responderPergunta(String pergunta){
+    public Flowable<ChatCompletionChunk> responderPergunta(String pergunta){
         var promptSistema = "Você é um chatbot de atendimento a clientes de um ecommerce e deve responder apenas perguntas relacionadas com o ecommerce";
         var dados = new DadosRequisicaoChatCompletion(promptSistema, pergunta);
         return client.enviarRequisicaoChatCompletion(dados);
